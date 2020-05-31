580104916cff5e3c5797030764b882fbd9528bee
Anselmo Lima <anselmo.dev@gmail.com>
2020-05-31 13:43:03 -0300

    Created: Recover Access and Set password pages.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,14 @@
+// PACOTE DE ÍCONES UTILIZADO
+// https://github.com/feathericons/feather#feather
+feather.replace();
+
+// VALIDAÇAÕ DE LOGIN
 $('#btn-login').click(function(){
   alert('Erro')
   return false;
 })
+
+// VALIDAÇÃO DE E-MAIL PARA RECUPERAÇÃO DE SENHA
+
+// VALIDAÇÃO DE NOVA SENHA
+
